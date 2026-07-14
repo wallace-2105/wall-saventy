62b4dfd0e440dafd7087db3de81e4eeea20e4af2
wallace-2105 <wallace.wcs83@gmail.com>
2026-07-14 18:03:59 -0300

    feat: calças a venda

--- script.js
+++ script.js
@@ -464,6 +464,9 @@ function renderProducts() {
 const featuredProductNames = [
   "Camiseta Essential Preta",
   "Jeans Slim",
+  "Jeans Reta",
+  "Jeans Black",
+  "Cargo Verde",
   "Jaqueta Jeans",
   "Sneaker Urban"
 ];
